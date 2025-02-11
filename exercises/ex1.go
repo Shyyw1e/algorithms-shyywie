b15971cb68a114cd6945b8c1fc852cb8826322b5
Egor Dimitrenko <edimitrenko05@mail.ru>
2025-02-11 19:04:52 +0300

    update

---
+++ exercises/ex1.go
@@ -0,0 +1,11 @@
+package exercises
+
+import(
+	"fmt"
+)
+
+func Exersice1(){
+	var reqNum int
+	fmt.Println("Write down integer")
+	fmt.Scan(&reqNum)
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"fmt"
+
+	"go-algorithms/exercises"
+)
+
+func main() {
+	fmt.Print("Ex.1:\n")
+	Exersice1()
+}
