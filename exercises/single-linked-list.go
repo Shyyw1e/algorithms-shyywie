638dfb43be1a0f4c978671906e808c2fea3edac6
Egor Dimitrenko <edimitrenko05@mail.ru>
2025-02-16 17:22:02 +0300

    update

--- exercises/single-linked-list.go
+++ exercises/single-linked-list.go
@@ -1,8 +1,8 @@
 package exercises
 
-import (
-	"fmt"
-)
+import "fmt"
+
+//"fmt"
 
 type Node struct {
 	data int
@@ -14,7 +14,7 @@ type SingleLinkedList struct {
 }
 
 func (list *SingleLinkedList) Append(value int) {
-	newNode := &Node{data : value}
+	newNode := &Node{data: value}
 	if list.head == nil {
 		list.head = newNode
 		return
@@ -33,7 +33,7 @@ func (list *SingleLinkedList) Delete(value int) {
 	}
 	if list.head.data == value {
 		list.head = list.head.next
-		return 
+		return
 	}
 	current := list.head
 	if current.next != nil && current.next.data != value {
@@ -42,4 +42,28 @@ func (list *SingleLinkedList) Delete(value int) {
 	if current.next != nil {
 		current.next = current.next.next
 	}
+}
+
+func (list *SingleLinkedList) Print() {
+	current := list.head
+	fmt.Printf("{%v", current.data)
+	current = current.next
+	for current.next != nil {
+		fmt.Printf(", %v", current.data)
+		current = current.next
+	}
+	fmt.Printf(", %v}", current.data)
+}
+
+func Exersice7() {
+	var list1 SingleLinkedList
+	var fLength int
+	fmt.Println("Enter list firstly length")
+	fmt.Scan(&fLength)
+	for i := 0; i < fLength; i++ {
+		var val int
+		fmt.Scan(&val)
+		list1.Append(val)
+	}
+	list1.Print()
 }
